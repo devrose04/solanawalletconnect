cac65edee13260ef12b7cfd953c27263a0eb74c8
devrose04 <webdevrose04@gmail.com>
2024-05-06 22:00:41 +0800

    upgrade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import { Button, Heading, VStack, useStatStyles } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import React, { useState, useEffect } from "react";
-import { useDisclosure, Modal, ModalBody, ModalOverlay, ModalHeader, ModalCloseButton, ModalFooter, ModalContent } from "@chakra-ui/react";
+import {
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalOverlay,
+  ModalHeader,
+  ModalCloseButton,
+  ModalFooter,
+  ModalContent,
+} from "@chakra-ui/react";
 import WalletModal from "./WalletModal";
 
 const home = "assets/home.jpg";
 const RouteComponent = () => {
   const [isMobile, setIsMobile] = useState(false);
   const [isPhantomInstalled, setIsPhantomInstalled] = useState(true);
+  
 
   useEffect(() => {
     const userAgent = window.navigator.userAgent.toLowerCase();
@@ -16,8 +26,11 @@ const RouteComponent = () => {
       userAgent.includes(keyword)
     );
     setIsMobile(isMobileDevice);
-    if (window?.phantom?.solana?.isPhantom) setIsPhantomInstalled(true);
-    else setIsPhantomInstalled(false);
+    if ('phantom' in window){
+      const provider = (window as any).phantom?.solana;
+      if (provider?.isPhantom) setIsPhantomInstalled(true);
+      else setIsPhantomInstalled(false);
+    }
   }, []); // Empty dependency array ensures useEffect runs only once when component mounts
 
   const handleRedirect = () => {
@@ -51,19 +64,16 @@ const RouteComponent = () => {
 };
 
 const IndexPage: React.FC = () => {
-  const [modalState, setModalState] = useState(false)
+  const [modalState, setModalState] = useState(false);
   const showModal = () => {
-    setModalState(true)
-  }
+    setModalState(true);
+  };
 
   return (
     <>
-      <VStack gap={8} mt={16} >
-        <WalletModal />
-      </VStack>
+      <WalletModal />
       <RouteComponent />
     </>
-
   );
-}
-export default IndexPage
+};
+export default IndexPage;
